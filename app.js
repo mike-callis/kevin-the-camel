4f4c99208ac0b5950867a2e3d52ba4f68721d99b
Michael Callis <mike.callis@energ.co.uk>
2019-03-16 20:20:54 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+var port = process.env.port || 3000;
+
+app.get('/', function(req, res){
+    res.send('Hello World - Kevin the camel here !!');
+});
+
+app.listen(port, function(){
+    console.log('Node.js Server: listening on port ' + port);
+});
