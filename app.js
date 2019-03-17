461e4492980b2a7ac5a456bb40d0f98352b2a72d
Michael Callis <mike.callis@energ.co.uk>
2019-03-17 12:30:55 +0000

    using -a in commit comand

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const app = express();
+//const app = express();
+const http = require('http');
+var fs = require('fs')
 
 var port = process.env.port || 3000;
 
-app.get('/', function(req, res){
-    res.send("Hello World - Kevin the camel here. I'm sat down !!!");
+var server = http.createServer(function(req, res){
+    console.log('request was made: ' + req.url);
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    myReadStream.pipe(res)
 });
 
-app.listen(port, function(){
+
+server.listen(port, function(){
     console.log('Node.js Server: listening on port ' + port);
 });
